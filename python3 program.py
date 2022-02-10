dd2a7a791e9094893becd4571b9732a381775af2
AleEysele24 <99201821+AleEysele24@users.noreply.github.com>
2022-02-09 20:47:44 -0600

    Add files via upload

---
+++ python3 program.py
@@ -0,0 +1,37 @@
+from sympy import product
+
+
+print('Hola desde la consola')
+# Impresion en patalla print('hola desde la consola')
+
+sum = 1+2 
+product = sum * 2
+print(product)
+
+#Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+type(distancia_a_alfa_centauri)
+
+#importamos la biblioteca
+from datetime import date 
+
+#obtenemos la fecha de hoy
+date.today()
+
+#Mostramos la fecha en la consola
+print(date.today())
+
+print("Today's date is : " + str(date.today()))
+
+#Escribe tu codigo aqui
+print("Bienvenido al programa de Bienvenida")
+name = input("Introduzca su nombre")
+print("Saludos: " + name)
+
+#Escribe tu codigo aqui
+print("Calculadora")
+first_number = input("Primer numero: ")
+second_number = input("Segundo numero: ")
+print(first_number + second_number)
+
