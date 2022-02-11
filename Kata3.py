c8b3b4ec44b6b38a75f54d39223ae96acd95f0b1
AleEysele24 <99201821+AleEysele24@users.noreply.github.com>
2022-02-11 00:26:49 -0600

    Add files via upload

---
+++ Kata3.py
@@ -0,0 +1,79 @@
+#Practica 1
+
+#a= 97
+#b= 55
+
+#if a < b:
+
+    #print(b)
+
+#Ejercicio 2
+#a= 93
+#b= 27
+
+#if a >= b:
+    #print(a)
+
+#Ejercicio 3
+
+#a= 93
+#b= 27
+
+#if a >= b:
+    #print(a)
+#else:
+    #print(b)
+
+#Ejercicio 4
+
+#a=25
+#b= 100/4
+
+#if  a == b:
+    
+       #print("25 cabe cuatro veces en 100")
+
+#else:  print("No cabe en el #100")
+
+#Ejercicio 5
+
+#from xml.dom.minidom import ElementInfo
+
+
+
+#Juan = 12
+
+#Abril = 12
+
+#if Juan > Abril:
+ 
+     #print( "Juan es el hermano mayor")
+
+
+#elif Juan < Abril: 
+    
+     #print (" Abril es la hermana mayor")
+
+#else :
+     #print("Los dos son gemelos tienen la misma edad xD")
+
+#Ejercicio 6
+
+a = 93
+b = 27
+
+if a == 34 and b == 34
+print( a + b)
+
+
+
+
+
+
+
+
+
+
+
+
+
