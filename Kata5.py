48bf30a8276c202285ee84e6cc35ac31584dc8fe
AleEysele24 <99201821+AleEysele24@users.noreply.github.com>
2022-02-12 09:57:25 -0600

    Add files via upload

---
+++ Kata5.py
@@ -0,0 +1,66 @@
+#Suma
+#answer = 30+12
+#print(answer)
+
+#Resta
+#difference = 30-12
+#print(difference)
+
+#Multiplicación
+#product = 30 *12
+#print(product)
+
+#División
+#quotient = 30/12
+#print(quotient)
+
+
+#Convertir de segundos a minutos
+
+#seconds = 1042
+#display_minutes = 1042 // 60
+
+
+#Paso 2
+#display_seconds = 1042 % 60
+#print(display_minutes)
+#print(display_seconds)
+
+#Jerarquia de Operaciónes
+
+#result_1 = 1032 + 26*2
+#result_2 = 1032 + (26*2)
+
+#print(result_1)
+#print(result_2)
+
+#Cadenas en números
+#demo_int = int('215')
+#print(demo_int)
+
+#Float
+#demo_float = float('215.3')
+#print(demo_float)
+
+#Valor Absoluto
+#a = 39 -16
+#b = 16 -39
+
+#print(abs(39 -16))
+#print(abs(16 - 39))
+
+#Redondeo
+#print(round(14.5))
+
+from math import ceil,floor
+
+round_up = ceil(12.5)
+print(round_up)
+
+round_down = floor(12.5)
+print(round_down)
+
+
+
+
+
